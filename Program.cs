54ea13be8af88563c2d4a5665ba54c9221472cfb
Вадим <vadimmm.m@ya.ru>
2020-09-20 15:13:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pr1S4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+
+            i16 = i32; 
+            i16 = db;  
+            i32 = i16; //неявное преобр short в int
+            i32 = db; 
+            db = i32; //неявное преобр int в double
+            db = i16; //неявное преобр short в int 
+        }
+    }
+}
